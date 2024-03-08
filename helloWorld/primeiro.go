a5143922c7d6bb0391406cc02af7a069f3676b45
David <joaovitorromanoteixeira@gmail.com>
2024-03-08 11:22:23 -0300

    aula1: hello world

---
+++ helloWorld/primeiro.go
@@ -0,0 +1,10 @@
+package main
+
+import("fmt")
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+//O package main define qual é o package principal do programa. Acompanhando ele, precisamos implementar a função main, que é o ponto de entrada do programa. O nome do package é sempre declarado na primeira linha de cada arquivo Go.
+//fmt É um pacote do GO que é utilizado para imprimir informações na tela, dentro desse pacote existem outras funções, a mais usada é o Println(  ) para imprimir informações na tela
