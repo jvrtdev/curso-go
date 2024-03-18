863f5784e5cb74684bd852cf0eaf02e678d381fd
David <joaovitorromanoteixeira@gmail.com>
2024-03-18 15:56:04 -0300

    aula: ponteiros

---
+++ ponteiros/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var variavel1 int = 10
+	var variavel2 int = variavel1
+
+	fmt.Println(variavel1, variavel2)
+
+	variavel1++
+	fmt.Println(variavel1, variavel2)
+
+	//ponteiro é uma referencia de memoria
+	var variavel3 int
+	var ponteiro *int
+
+	variavel3 = 100
+	ponteiro = &variavel3
+	fmt.Println(variavel3, ponteiro) //desreferenciacao
+}
